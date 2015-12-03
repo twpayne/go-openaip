b8733cae4bc7af7610464d64d2becf8c2cf4d0c5
Tom Payne <twpayne@gmail.com>
2015-12-03 22:57:42 +0100

    Initial commit

---
+++ openaip.go
@@ -0,0 +1,106 @@
+// Package openaip decodes http://www.openaip.net/ airspace files.
+package openaip
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
+)
+
+// An Alt represents an altitude.
+type Alt struct {
+	Unit  string
+	Value int
+}
+
+// An AltLimit represents an altitude limit.
+type AltLimit struct {
+	Reference string `xml:"REFERENCE,attr"`
+	Value     Alt    `xml:"ALT"`
+}
+
+// A Polygon represents a two-dimensional polygon.
+type Polygon struct {
+	Coords [][2]float64 // Longitude, latitude
+}
+
+// An Airspace represents a single airspace.
+type Airspace struct {
+	Category       string    `xml:"CATEGORY,attr"`
+	Version        string    `xml:"VERSION"`
+	Id             int       `xml:"ID"`
+	Country        string    `xml:"COUNTRY"`
+	Name           string    `xml:"NAME"`
+	AltLimitTop    AltLimit  `xml:"ALTLIMIT_TOP"`
+	AltLimitBottom AltLimit  `xml:"ALTLIMIT_BOTTOM"`
+	Polygons       []Polygon `xml:"GEOMETRY>POLYGON"`
+}
+
+// An OpenAIP represents version information and zero or more Airspaces.
+type OpenAIP struct {
+	Version    string     `xml:"VERSION,attr"`
+	DataFormat string     `xml:"DATAFORMAT,attr"`
+	Airspaces  []Airspace `xml:"AIRSPACES>ASP"`
+}
+
+func (a *Alt) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	for _, attr := range start.Attr {
+		if attr.Name.Local == "UNIT" {
+			a.Unit = attr.Value
+		}
+	}
+	var value string
+	if err := d.DecodeElement(&value, &start); err != nil {
+		return err
+	}
+	var err error
+	a.Value, err = strconv.Atoi(value)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p *Polygon) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var value string
+	if err := d.DecodeElement(&value, &start); err != nil {
+		return err
+	}
+	css := strings.Split(value, ",")
+	if len(css) < 3 {
+		return fmt.Errorf("expected at least 3 coordinates, got %d: %#v", len(css), css)
+	}
+	coords := make([][2]float64, len(css))
+	for i, cs := range css {
+		fields := strings.Fields(cs)
+		if len(fields) != 2 {
+			return fmt.Errorf("expected two values, got %d: %v", len(fields), cs)
+		}
+		var err error
+		coords[i][0], err = strconv.ParseFloat(fields[0], 64)
+		if err != nil {
+			return err
+		}
+		coords[i][1], err = strconv.ParseFloat(fields[1], 64)
+		if err != nil {
+			return err
+		}
+	}
+	p.Coords = append(p.Coords, coords...)
+	return nil
+}
+
+// Read reads an OpenAIP from r.
+func Read(r io.Reader) (*OpenAIP, error) {
+	var oa OpenAIP
+	d := xml.NewDecoder(r)
+	if err := d.Decode(&oa); err != nil {
+		return nil, err
+	}
+	if oa.DataFormat != "1.1" {
+		return nil, fmt.Errorf("openaip.Read: unsupported data format: %v", oa.DataFormat)
+	}
+	return &oa, nil
+}
---
+++ openaip_test.go
@@ -0,0 +1,202 @@
+package openaip
+
+import (
+	"bytes"
+	"encoding/xml"
+	"reflect"
+	"testing"
+)
+
+func TestDecodeAlt(t *testing.T) {
+	for _, tc := range []struct {
+		s    string
+		want Alt
+	}{
+		{`<ALT UNIT="FL">100</ALT>`, Alt{Unit: "FL", Value: 100}},
+		{`<ALT UNIT="FL">75</ALT>`, Alt{Unit: "FL", Value: 75}},
+	} {
+		d := xml.NewDecoder(bytes.NewBufferString(tc.s))
+		var got Alt
+		if err := d.Decode(&got); err != nil {
+			t.Errorf("decoding %#v returns %v, want nil", tc.s, err)
+			continue
+		}
+		if got != tc.want {
+			t.Errorf("decoding %#v yields %#v, want %#v", tc.s, got, tc.want)
+		}
+	}
+}
+
+func TestDecodeAltLimit(t *testing.T) {
+	for _, tc := range []struct {
+		s    string
+		want AltLimit
+	}{
+		{
+			s:    `<ALTLIMIT_TOP REFERENCE="STD"><ALT UNIT="FL">100</ALT></ALTLIMIT_TOP>`,
+			want: AltLimit{Reference: "STD", Value: Alt{Unit: "FL", Value: 100}},
+		},
+		{
+			s:    `<ALTLIMIT_BOTTOM REFERENCE="STD"><ALT UNIT="FL">75</ALT></ALTLIMIT_BOTTOM>`,
+			want: AltLimit{Reference: "STD", Value: Alt{Unit: "FL", Value: 75}},
+		},
+	} {
+		d := xml.NewDecoder(bytes.NewBufferString(tc.s))
+		var got AltLimit
+		if err := d.Decode(&got); err != nil {
+			t.Errorf("decoding %#v returns %v, want nil", tc.s, err)
+			continue
+		}
+		if got != tc.want {
+			t.Errorf("decoding %#v yields %#v, want %#v", tc.s, got, tc.want)
+		}
+	}
+}
+
+func TestDecodePolygon(t *testing.T) {
+	for _, tc := range []struct {
+		s    string
+		want Polygon
+	}{
+		{
+			s: `<POLYGON>` +
+				`9.6255555555556 48.5625, ` +
+				`9.8477777777778 48.659444444444, ` +
+				`9.8488590649036 48.671520456103, ` +
+				`9.8491357659723 48.676179034769, ` +
+				`9.8494899239756 48.688273884048, ` +
+				`9.8494860905378 48.692936043918, ` +
+				`9.8477777777778 48.705, ` +
+				`9.9372222222222 48.714166666667, ` +
+				`9.9380313248976 48.696291564689, ` +
+				`9.937831067577 48.678408978203, ` +
+				`9.9366228021006 48.660543713921, ` +
+				`9.9344092727478 48.64272052819, ` +
+				`9.9319444444444 48.630833333333, ` +
+				`9.6391666666667 48.496388888889, ` +
+				`9.6255555555556 48.5625` +
+				`</POLYGON>`,
+			want: Polygon{
+				Coords: [][2]float64{
+					{9.6255555555556, 48.5625},
+					{9.8477777777778, 48.659444444444},
+					{9.8488590649036, 48.671520456103},
+					{9.8491357659723, 48.676179034769},
+					{9.8494899239756, 48.688273884048},
+					{9.8494860905378, 48.692936043918},
+					{9.8477777777778, 48.705},
+					{9.9372222222222, 48.714166666667},
+					{9.9380313248976, 48.696291564689},
+					{9.937831067577, 48.678408978203},
+					{9.9366228021006, 48.660543713921},
+					{9.9344092727478, 48.64272052819},
+					{9.9319444444444, 48.630833333333},
+					{9.6391666666667, 48.496388888889},
+					{9.6255555555556, 48.5625},
+				},
+			},
+		},
+	} {
+		d := xml.NewDecoder(bytes.NewBufferString(tc.s))
+		var got Polygon
+		if err := d.Decode(&got); err != nil {
+			t.Errorf("decoding %#v returns %v, want nil", tc.s, err)
+			continue
+		}
+		if !reflect.DeepEqual(got, tc.want) {
+			t.Errorf("decoding %#v yields %#v, want %#v", tc.s, got, tc.want)
+		}
+	}
+}
+
+func TestRead(t *testing.T) {
+	for _, tc := range []struct {
+		s    string
+		want OpenAIP
+	}{
+		{
+			s: `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>` +
+				`<OPENAIP VERSION="367810a0f94887bf79cd9432d2a01142b0426795" DATAFORMAT="1.1">` +
+				`<AIRSPACES>` +
+				`<ASP CATEGORY="WAVE">` +
+				`<VERSION>367810a0f94887bf79cd9432d2a01142b0426795</VERSION>` +
+				`<ID>18024</ID>` +
+				`<COUNTRY>DE</COUNTRY>` +
+				`<NAME>ALB-OST</NAME>` +
+				`<ALTLIMIT_TOP REFERENCE="STD">` +
+				`<ALT UNIT="FL">100</ALT>` +
+				`</ALTLIMIT_TOP>` +
+				`<ALTLIMIT_BOTTOM REFERENCE="STD">` +
+				`<ALT UNIT="FL">75</ALT>` +
+				`</ALTLIMIT_BOTTOM>` +
+				`<GEOMETRY>` +
+				`<POLYGON>` +
+				`9.6255555555556 48.5625, ` +
+				`9.8477777777778 48.659444444444, ` +
+				`9.8488590649036 48.671520456103, ` +
+				`9.8491357659723 48.676179034769, ` +
+				`9.8494899239756 48.688273884048, ` +
+				`9.8494860905378 48.692936043918, ` +
+				`9.8477777777778 48.705, ` +
+				`9.9372222222222 48.714166666667, ` +
+				`9.9380313248976 48.696291564689, ` +
+				`9.937831067577 48.678408978203, ` +
+				`9.9366228021006 48.660543713921, ` +
+				`9.9344092727478 48.64272052819, ` +
+				`9.9319444444444 48.630833333333, ` +
+				`9.6391666666667 48.496388888889, ` +
+				`9.6255555555556 48.5625` +
+				`</POLYGON>` +
+				`</GEOMETRY>` +
+				`</ASP>` +
+				`</AIRSPACES>` +
+				`</OPENAIP>`,
+			want: OpenAIP{
+				Version:    "367810a0f94887bf79cd9432d2a01142b0426795",
+				DataFormat: "1.1",
+				Airspaces: []Airspace{
+					{
+						Category:       "WAVE",
+						Version:        "367810a0f94887bf79cd9432d2a01142b0426795",
+						Id:             18024,
+						Country:        "DE",
+						Name:           "ALB-OST",
+						AltLimitTop:    AltLimit{Reference: "STD", Value: Alt{Unit: "FL", Value: 100}},
+						AltLimitBottom: AltLimit{Reference: "STD", Value: Alt{Unit: "FL", Value: 75}},
+						Polygons: []Polygon{
+							{
+								Coords: [][2]float64{
+									{9.6255555555556, 48.5625},
+									{9.8477777777778, 48.659444444444},
+									{9.8488590649036, 48.671520456103},
+									{9.8491357659723, 48.676179034769},
+									{9.8494899239756, 48.688273884048},
+									{9.8494860905378, 48.692936043918},
+									{9.8477777777778, 48.705},
+									{9.9372222222222, 48.714166666667},
+									{9.9380313248976, 48.696291564689},
+									{9.937831067577, 48.678408978203},
+									{9.9366228021006, 48.660543713921},
+									{9.9344092727478, 48.64272052819},
+									{9.9319444444444, 48.630833333333},
+									{9.6391666666667, 48.496388888889},
+									{9.6255555555556, 48.5625},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	} {
+		d := xml.NewDecoder(bytes.NewBufferString(tc.s))
+		var got OpenAIP
+		if err := d.Decode(&got); err != nil {
+			t.Errorf("decoding %#v returns %v, want nil", tc.s, err)
+			continue
+		}
+		if !reflect.DeepEqual(got, tc.want) {
+			t.Errorf("decoding %#v yields %#v, want %#v", tc.s, got, tc.want)
+		}
+	}
+}
